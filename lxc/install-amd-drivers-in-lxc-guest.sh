e024e3f6e41cb389902ba5e198920eeef8826745
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2025-09-26 17:21:03 +0200

    removed sudo

--- lxc/install-amd-drivers-in-lxc-guest.sh
+++ lxc/install-amd-drivers-in-lxc-guest.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Add ROCm repository
-wget -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
-echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/6.2.4 jammy main" | sudo tee /etc/apt/sources.list.d/rocm.list
+wget -O - https://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
+echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/6.2.4 jammy main" | tee /etc/apt/sources.list.d/rocm.list
 
 apt update
 apt install -y rocm-dev rocm-libs rocm-utils --no-install-recommends
 
 # Install AMD Container Toolkit for ROCm support
-curl -fsSL https://rocm.docs.amd.com/rocm.gpg.key | sudo apt-key add -
-echo "deb [arch=amd64] https://rocm.docs.amd.com/apt/6.2.4 jammy main" | sudo tee -a /etc/apt/sources.list.d/rocm.list
+curl -fsSL https://rocm.docs.amd.com/rocm.gpg.key | apt-key add -
+echo "deb [arch=amd64] https://rocm.docs.amd.com/apt/6.2.4 jammy main" | tee -a /etc/apt/sources.list.d/rocm.list
 apt update
 apt -y install amd-container-toolkit
 
