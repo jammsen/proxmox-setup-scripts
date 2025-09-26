f8dd9cce9212815b90cb07d9fd2d6a6896c81a80
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2025-09-26 17:18:45 +0200

    added amd-driver-setup

---
+++ lxc/install-amd-drivers-in-lxc-guest.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Add ROCm repository
+wget -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
+echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/6.2.4 jammy main" | sudo tee /etc/apt/sources.list.d/rocm.list
+
+apt update
+apt install -y rocm-dev rocm-libs rocm-utils --no-install-recommends
+
+# Install AMD Container Toolkit for ROCm support
+curl -fsSL https://rocm.docs.amd.com/rocm.gpg.key | sudo apt-key add -
+echo "deb [arch=amd64] https://rocm.docs.amd.com/apt/6.2.4 jammy main" | sudo tee -a /etc/apt/sources.list.d/rocm.list
+apt update
+apt -y install amd-container-toolkit
+
+# Configure Docker for AMD GPU access
+amd-ctk runtime configure --runtime=docker
+systemctl daemon-reload
+systemctl restart docker
