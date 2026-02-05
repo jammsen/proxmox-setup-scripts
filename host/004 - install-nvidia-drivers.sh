dcfcc43e49e2c570591d7cbbca0cf1ab21cc4c30
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2026-02-05 10:53:22 +0100

    Update CUDA keyring download URL for Debian 13 - See: https://github.com/NVIDIA/cuda-repo-management/issues/30

--- host/004 - install-nvidia-drivers.sh
+++ host/004 - install-nvidia-drivers.sh
@@ -6,7 +6,7 @@ apt update
 echo ">>> Installing Proxmox headers for current kernel"
 apt install -y proxmox-headers-"$(uname -r)"
 echo ">>> Downloading and installing NVIDIA CUDA keyring"
-wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 rm cuda-keyring_1.1-1_all.deb
 apt update
